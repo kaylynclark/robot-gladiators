fefea704baa114449813a7c64674fd735e62472b
Ladybug <clark.kaylyn97@gmail.com>
2022-01-07 19:46:22 -0800

    added health and attack variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,32 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
-function fight() {
-    window.alert("The fight has begun!");
+var playerHealth = 100;
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");
 }
-// fight();
+var playerAttack = 10;
+
+/* You can also log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth); */
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // alert players they are starting the round
+    window.alert("Welcome to Robot Gladiators!")
+
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+    //Log a resulting message to the console so we know it worked.
+    console.log(
+    playerName  +  "attacked"  +  enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
+    );
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that to update the value in the 'playerHealth variable.
+    playerHealth = playerHealth - enemyAttack;
+    //Log a resulting message to the console so we know it worked.
+    console.log(
+        enemyName + "attacked" + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+    );
+};
+fight();
