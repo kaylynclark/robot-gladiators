90753a9765b2161e6ec43adff90a51a58a6befdc
Ladybug <clark.kaylyn97@gmail.com>
2022-01-07 18:58:53 -0800

    added some variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,10 @@
 var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
 function fight() {
     window.alert("The fight has begun!");
 }
-fight();
+// fight();
